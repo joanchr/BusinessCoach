b1063b463a3c0f0d531ccc3bcbd1ae9478aad22b
joanchr <joanchristib@gmail.com>
2021-05-05 10:50:34 +0700

    navigation bar animation

--- master.js
+++ master.js
@@ -17,3 +17,12 @@ $(window).on('resize', function(event){
         menu.style.display = "none";
     }
 });
+
+window.addEventListener("scroll", function(){
+    var navigationBar = document.querySelector("nav");
+    if(window.pageYOffset > 50){
+        navigationBar.style.boxShadow = "0px 5px 10px rgba(0, 0, 0, 0.2)";
+    }else{
+        navigationBar.style.boxShadow = "none";
+    }
+});
